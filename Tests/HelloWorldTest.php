c5690ed7a36facbc1010e376646c3a922a10c348
mirandaLopezAbraham1 <abrancito09@hotmail.com>
2021-07-17 23:37:54 -0500

    void

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -7,7 +7,7 @@ class HelloWorldTest extends TestCase
      */
     private $pdo;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
