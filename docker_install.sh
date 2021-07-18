613013ca99244c90e6e40a5c24a6f1e39c05f1d9
mirandaLopezAbraham1 <abrancito09@hotmail.com>
2021-07-18 00:37:35 -0500

    utils cambio

--- docker_install.sh
+++ docker_install.sh
@@ -9,7 +9,7 @@ set -xe
 apt-get update -yqq
 apt-get install git -yqq
 apt-get install libpq-dev
-apt-get install utils
+apt-get install -y apt-utils && apt-get install -y curl
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit "https://phar.phpunit.de/phpunit.phar"
