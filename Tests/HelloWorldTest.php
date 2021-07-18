518fa16798e6203dbbab36650a0878986786ed29
mirandaLopezAbraham1 <abrancito09@hotmail.com>
2021-07-18 00:03:08 -0500

    hola mundo

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -7,12 +7,12 @@ class HelloWorldTest extends TestCase
      */
     private $pdo;
 
-    // public function setUp(): void
-    // {
-    //     $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
-    //     $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //     $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
-    // }
+    public function setUp(): void
+    {
+        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
+    }
 
     public function tearDown(): void
     {
