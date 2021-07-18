03f736336cbbc7b5b5fe6c27d535dbbf5e4ceb4f
jhespinoza <jhespinoza@espe.edu.ec>
2021-07-18 13:30:48 -0500

    class correction

--- Calc.php
+++ Calc.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace App\Classes;
-
 class Calc
 {
     public function sum(int $num1, int $num2)
--- Tests/CalcTest.php
+++ Tests/CalcTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use App\Classes\Calc;
 
 class CalcTest extends TestCase
 {
